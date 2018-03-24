4fe12bf70625add009b0604a1d9fcf27cce9b923
Henrique Leal <hm.leal@hotmail.com>
2018-03-24 16:33:27 +0000

    feature: Update migration filename

--- pydbmate/__main__.py
+++ pydbmate/__main__.py
@@ -1,5 +1,29 @@
-def main():
-    print('Hello pydbmate')
+"""Pydbmate
+
+Usage:
+  main.py new <filename>
+  main.py migrate
+  main.py (-h | --help)
+  main.py --version
+
+Options:
+  -h --help     Show this screen.
+  --version     Show version.
+"""
+from docopt import docopt
+
+from pydbmate import db
+
+
+def main(args=None):
+    args = docopt(__doc__, version='Pydbmate')
+
+    if args['new']:
+        db.new_migration(args['<filename>'])
+
+    if args['migrate']:
+        pass
+        # db.parse_migration_file('filename.sql')
 
 
 if __name__ == '__main__':
--- pydbmate/db.py
+++ pydbmate/db.py
@@ -1,6 +1,8 @@
 import os
 import re
 
+from datetime import datetime
+
 
 DATABASE_URL = ''
 DEFAULT_MIGRATION_DIR = './db/migrations'
@@ -18,11 +20,17 @@ def new_migration(name: str):
     """Creates a new empty migration file"""
     ensure_dir()
 
-    filename = '{0}.sql'.format(name)
+    filename = generate_migration_file_name(name)
     filepath = os.path.join(DEFAULT_MIGRATION_DIR, filename)
 
     with open(filepath, 'w+') as f:
         f.write(MIGRATION_TEMPLATE)
+
+
+def generate_migration_file_name(suffix: str):
+    pattern = '{:%Y%m%d_%H%M}_{}.sql'
+
+    return pattern.format(datetime.now(), suffix)
 
 
 def parse_migration_file(filename: str):
